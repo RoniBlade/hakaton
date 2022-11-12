c88e398bcebbae6ac161571d5bc5fac36cf4a4e9
RoniBlade <78499428+RoniBlade@users.noreply.github.com>
2022-11-12 20:26:10 +0300

    Add files via upload

---
+++ js/video-script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    let video = document.querySelector('.video');
+    let back = document.querySelector('.back');
+
+    video.addEventListener('click', function() {
+
+        if(video.classList.contains('ready')){
+            return;
+        }
+
+        video.classList.add('ready');
+        back.parentElement.removeChild(back);
+
+        video.insertAdjacentHTML('afterbegin', '<iframe src="https://www.youtube.com/embed/JqhLqBPwjvo?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+
+
+
+
+
+    });
+
+});
+
