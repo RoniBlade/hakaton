5fb431a483da553f1b4ef50b98a29ad18989cf30
RoniBlade <78499428+RoniBlade@users.noreply.github.com>
2022-11-13 08:57:35 +0300

    Add files via upload

--- js/video-script.js
+++ js/video-script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function() {
         video.classList.add('ready');
         back.parentElement.removeChild(back);
 
-        video.insertAdjacentHTML('afterbegin', '<iframe width="560" height="315" src="https://www.youtube.com/embed/Lj-Rua0KEE0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+        video.insertAdjacentHTML('afterbegin', '<iframe width="560" height="315" src="https://www.youtube.com/embed/Lj-Rua0KEE0?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 
 
 
